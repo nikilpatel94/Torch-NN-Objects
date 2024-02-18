cadaa6a49455e6000056229b565c7605037c3908
Nikil Patel <patel.nikil1994@gmail.com>
2024-02-18 01:00:09 +0000

    Added splitter for train,validation,test

--- helper_functions.py
+++ helper_functions.py
@@ -7,3 +7,24 @@ def simple_batch_maker(batch_size:int,seq_len:int,index:int,data:list,torch_tans
     batch[i] = torch.tensor( data[index:index+seq_len],dtype = torch.float ).unsqueeze(dim=0)
     index = index+seq_len
   return batch
+
+def train_validation_test_splitter(data:list,train_test_split:float,valid_split=None,verbose=False):
+  train_len = int(len(data)*train_test_split)
+  data_test = data[train_len:]
+  #Check if validation split is requested 
+  if(valid_split):
+    valid_len = train_len-int(train_len*valid_split)
+    data_train = data[:train_len][:valid_len]
+    data_validation = data[:train_len][valid_len:]
+    assert len(data) == (len(data_train)+len(data_validation)+len(data_test))
+    if verbose:
+      print('Length of Training,Validation,Testing\n',len(data_train),len(data_validation),len(data_test))
+    return data_train,data_validation,data_test
+  
+  else:
+    data_train = data[:train_len]
+    assert len(data) == (len(data_train)+len(data_test))
+    if verbose:
+      print('Length of Training,Testing:\n',len(data_train),len(data_test))
+    return data_train,data_test
+  
